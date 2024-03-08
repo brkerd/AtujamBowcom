76b699d7c7e28be45b45551c55d054a4d0c9ebac
brkerd <49274471+brkerd@users.noreply.github.com>
2024-03-08 19:22:26 +0300

    Movement Logic Completed
    Movement logic completed with slight issues that can be fixed (Character doesn't jump as high as intended when jump is pressed in a small timeframe between grounded is true and y velocity is 0)
    Made Character and the grid prefabs for easier usage

--- Assets/Player Scripts/PlayerMovement.cs
+++ Assets/Player Scripts/PlayerMovement.cs
@@ -1,52 +1,99 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
-    public float speed;
-    public float drag = 0.9f;
     public Rigidbody2D body;
     public BoxCollider2D groundCheck;
     public LayerMask groundMask;
+
+    public float acceleration;
+    [Range(0f, 1f)]
+    public float groundDecay;
+    public float maxGroundSpeed;
+
+    public float jumpSpeed;
+
     public bool grounded;
-    
+    float xInput;
+    float yInput;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        float xInput = Input.GetAxis("Horizontal");
-        float yInput = Input.GetAxis("Vertical");
-
-        if (Mathf.Abs(xInput)>0)
-        {
-            body.velocity = new Vector2(xInput*speed, body.velocity.y);
-        }
-        if (Mathf.Abs(yInput)>0)
-        {
-            body.velocity = new Vector2(body.velocity.x, yInput*speed);
-        }
-
-
-        Vector2 direction = new Vector2(xInput,yInput).normalized;
-        body.velocity = direction*speed;
+        CheckInput();
+        HandleJump();
     }
 
-    void FixedUpdate() {
+    void CheckInput()
+    {
+        xInput = Input.GetAxis("Horizontal");
+        yInput = Input.GetAxis("Vertical");
+    }
+    void HandleXInput()
+    {
+
+
+        if (Mathf.Abs(xInput) > 0)
+        {
+            float increment = xInput * acceleration;
+            float newSpeed = Mathf.Clamp(body.velocity.x + increment, -maxGroundSpeed, maxGroundSpeed);
+            body.velocity = new Vector2(newSpeed, body.velocity.y);
+
+            FaceInput();
+
+        }
+
+    }
+
+    void FaceInput()
+    {
+        float direction = Mathf.Sign(xInput);
+        transform.localScale = new Vector3(-direction, 1, 1);
+    }
+
+    void FixedUpdate()
+    {
         CheckGround();
-        if (grounded){
-            body.velocity *= drag;
+        HandleXInput();
+        ApplyFriction();
+    }
+
+    void ApplyFriction()
+    {
+        // if (body.velocity.y != 0)
+        // {
+        //     Debug.Log(body.velocity.y);
+        // }
+        
+        if (grounded && xInput == 0 && (body.velocity.y <= 0f || body.velocity.y >= 0f))
+        {
+            body.velocity *= groundDecay;
         }
     }
 
-    void CheckGround(){
+    void HandleJump()
+    {
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            float idleJump = body.velocity.y == 0 ? jumpSpeed / groundDecay : jumpSpeed;
+            body.velocity = new Vector2(body.velocity.x, idleJump);
+        }
+    }
+
+    void CheckGround()
+    {
         grounded = Physics2D.OverlapAreaAll(groundCheck.bounds.min, groundCheck.bounds.max, groundMask).Length > 0;
     }
+
+
 }
